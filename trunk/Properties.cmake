705d996116cae4529d31e0f50bc736991818173d
stevo.bellus@gmail.com <stevo.bellus@gmail.com@93acecfd-95c6-5e0d-11b2-f2f7f10c5782>
2013-06-17 13:19:49 +0000

    Dependencies description syntax changed

--- trunk/Properties.cmake
+++ trunk/Properties.cmake
@@ -1,7 +1,7 @@
 # project info
 set(NAME             "HelloWorld")
 set(TYPE             "Executable")
-set(VERSION_MAJOR    "1")
+set(VERSION_MAJOR    "2")
 set(VERSION_MINOR    "0")
 set(VERSION_PATCH    "0")
 set(DESCRIPTION      "HelloWorld application prints hello world on console.")
@@ -9,8 +9,8 @@ set(MAINTAINER       "stevo.bellus@gmail.com")
 
 # project dependecies
 set(DEPENDENCIES
-http://sbe-documentationsystem.googlecode.com/svn/tags/rel_1_0_0
-http://sbe-cpputestlibrary.googlecode.com/svn/tags/rel_1_0_0
+DEPENDENCY URL http://sbe-documentationsystem.googlecode.com/svn/tags/rel_2_0_0
+DEPENDENCY URL http://sbe-cpputestlibrary.googlecode.com/svn/tags/rel_1_0_0 
 )
 
 
