96a06aa6dfd4e588b7ed64dafa6adb33ebbb10f3
stevo.bellus@gmail.com <stevo.bellus@gmail.com@93acecfd-95c6-5e0d-11b2-f2f7f10c5782>
2013-06-28 12:48:51 +0000

    New SBE used. Supports DECLSPEC handling.

--- trunk/Properties.cmake
+++ trunk/Properties.cmake
@@ -3,12 +3,12 @@ set(NAME             "HelloWorld")
 set(TYPE             "Executable")
 set(VERSION_MAJOR    "2")
 set(VERSION_MINOR    "0")
-set(VERSION_PATCH    "0")
+set(VERSION_PATCH    "1")
 set(DESCRIPTION      "HelloWorld application prints hello world on console.")
 set(MAINTAINER       "stevo.bellus@gmail.com")
 
 # project dependecies
 set(DEPENDENCIES
-DEPENDENCY URL http://sbe-documentationsystem.googlecode.com/svn/tags/rel_2_0_3
-DEPENDENCY URL http://sbe-cpputestlibrary.googlecode.com/svn/tags/rel_1_0_0
+DEPENDENCY URL http://sbe-documentationsystem.googlecode.com/svn/tags/rel_3_0_1
+DEPENDENCY URL http://sbe-cpputestlibrary.googlecode.com/svn/tags/rel_2_0_7
 )
