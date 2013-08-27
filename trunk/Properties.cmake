d8c1c4ae382933f1cbc29418243ec7af3a8f968a
stevo.bellus@gmail.com <stevo.bellus@gmail.com@93acecfd-95c6-5e0d-11b2-f2f7f10c5782>
2013-08-27 14:36:45 +0000

    CppUTest with MSVC support

--- trunk/Properties.cmake
+++ trunk/Properties.cmake
@@ -3,12 +3,12 @@ set(NAME             "HelloWorld")
 set(TYPE             "Executable")
 set(VERSION_MAJOR    "2")
 set(VERSION_MINOR    "0")
-set(VERSION_PATCH    "2")
+set(VERSION_PATCH    "3")
 set(DESCRIPTION      "HelloWorld application prints hello world on console.")
 set(MAINTAINER       "stevo.bellus@gmail.com")
 
 # project dependecies
 set(DEPENDENCIES
 DEPENDENCY URL http://sbe-documentationsystem.googlecode.com/svn/tags/rel_3_0_2_9
-DEPENDENCY URL http://sbe-cpputestlibrary.googlecode.com/svn/tags/rel_2_0_9
+DEPENDENCY URL http://sbe-cpputestlibrary.googlecode.com/svn/tags/rel_2_0_10
 )
